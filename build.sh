5a70e40660615a9d0199f4b29be0f7189dd6840b
Corentin Gay <gay@adacore.com>
2018-07-17 10:09:22 +0200

    clean build script

--- build.sh
+++ build.sh
@@ -3,20 +3,19 @@
 STYLE=zenburn
 # In the case where we include code, we have nice syntax highlighting.
 
-rm report.tex
-
 dot -Tpng graph.dot -o plan.png
 
 convert plan.png -fuzz 1% -transparent white plan.png
 
 pandoc \
 	--latex-engine=pdflatex\
-	--filter pandoc-citeproc --bibliography=biblio.bib\
+	--bibliography=biblio.bib\
 	--number-sections\
 	--from=markdown+grid_tables+pipe_tables\
 	--template=./template.tex\
 	--toc\
 	--variable urlcolor=blue\
+	--metadata link-citations=true\
 	--highlight-style=$STYLE\
         report.md\
         -s -o report.tex
@@ -24,15 +23,3 @@ pandoc \
 pdflatex report.tex -o report.pdf
 makeglossaries report
 pdflatex report.tex -o report.pdf
-
-#pandoc \
-#	--latex-engine=pdflatex\
-#	--filter pandoc-citeproc --bibliography=biblio.bib\
-#	--number-sections\
-#	--from=markdown+grid_tables+pipe_tables\
-#        -fmarkdown-implicit_figures\
-#	--template=./template.tex\
-#	--toc\
-#	--highlight-style=$STYLE\
-#        report.md\
-#        -o report.pdf
